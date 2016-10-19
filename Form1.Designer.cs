f8e469826154b9f48a4c9f90ddfc02335f17097e
EveKS <vikkes@yandex.ru>
2016-10-19 14:26:43 +0700

    добавил проэкт

---
+++ Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace Xml_To_Excel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label8 = new System.Windows.Forms.Label();
+            this.tbSelectExel_1 = new System.Windows.Forms.TextBox();
+            this.tbSelectExel_0 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbBool = new System.Windows.Forms.TextBox();
+            this.tbCopyFrom = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnSavePath = new System.Windows.Forms.Button();
+            this.btnSelectExel = new System.Windows.Forms.Button();
+            this.btnSelectXml = new System.Windows.Forms.Button();
+            this.tbSaveName = new System.Windows.Forms.TextBox();
+            this.tbSavePath = new System.Windows.Forms.TextBox();
+            this.tbSelectExel = new System.Windows.Forms.TextBox();
+            this.tbSelectXml = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label8
+            // 
+            this.label8.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(458, 46);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(84, 13);
+            this.label8.TabIndex = 81;
+            this.label8.Text = "Select baze size";
+            // 
+            // tbSelectExel_1
+            // 
+            this.tbSelectExel_1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbSelectExel_1.Location = new System.Drawing.Point(528, 71);
+            this.tbSelectExel_1.Name = "tbSelectExel_1";
+            this.tbSelectExel_1.Size = new System.Drawing.Size(61, 20);
+            this.tbSelectExel_1.TabIndex = 80;
+            // 
+            // tbSelectExel_0
+            // 
+            this.tbSelectExel_0.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbSelectExel_0.Location = new System.Drawing.Point(461, 71);
+            this.tbSelectExel_0.Name = "tbSelectExel_0";
+            this.tbSelectExel_0.Size = new System.Drawing.Size(61, 20);
+            this.tbSelectExel_0.TabIndex = 79;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(42, 170);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(33, 13);
+            this.label7.TabIndex = 75;
+            this.label7.Text = "From:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(42, 151);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 13);
+            this.label5.TabIndex = 73;
+            this.label5.Text = "Cells to copy";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(42, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 13);
+            this.label4.TabIndex = 72;
+            this.label4.Text = "Cells for comparison";
+            // 
+            // tbBool
+            // 
+            this.tbBool.Location = new System.Drawing.Point(81, 117);
+            this.tbBool.Name = "tbBool";
+            this.tbBool.Size = new System.Drawing.Size(61, 20);
+            this.tbBool.TabIndex = 62;
+            // 
+            // tbCopyFrom
+            // 
+            this.tbCopyFrom.Location = new System.Drawing.Point(81, 167);
+            this.tbCopyFrom.Name = "tbCopyFrom";
+            this.tbCopyFrom.Size = new System.Drawing.Size(61, 20);
+            this.tbCopyFrom.TabIndex = 52;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 204);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 51;
+            this.label3.Text = "Select path save";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 50;
+            this.label2.Text = "Select";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(225, 249);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 49;
+            this.label1.Text = "Input name save:";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Location = new System.Drawing.Point(514, 246);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 44);
+            this.btnSave.TabIndex = 48;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnSavePath
+            // 
+            this.btnSavePath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSavePath.Location = new System.Drawing.Point(514, 220);
+            this.btnSavePath.Name = "btnSavePath";
+            this.btnSavePath.Size = new System.Drawing.Size(75, 20);
+            this.btnSavePath.TabIndex = 47;
+            this.btnSavePath.Text = "SavePath";
+            this.btnSavePath.UseVisualStyleBackColor = true;
+            // 
+            // btnSelectExel
+            // 
+            this.btnSelectExel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelectExel.Location = new System.Drawing.Point(362, 71);
+            this.btnSelectExel.Name = "btnSelectExel";
+            this.btnSelectExel.Size = new System.Drawing.Size(75, 20);
+            this.btnSelectExel.TabIndex = 46;
+            this.btnSelectExel.Text = "Exel";
+            this.btnSelectExel.UseVisualStyleBackColor = true;
+            // 
+            // btnSelectXml
+            // 
+            this.btnSelectXml.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelectXml.Location = new System.Drawing.Point(362, 44);
+            this.btnSelectXml.Name = "btnSelectXml";
+            this.btnSelectXml.Size = new System.Drawing.Size(75, 20);
+            this.btnSelectXml.TabIndex = 45;
+            this.btnSelectXml.Text = "Xml";
+            this.btnSelectXml.UseVisualStyleBackColor = true;
+            // 
+            // tbSaveName
+            // 
+            this.tbSaveName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbSaveName.Location = new System.Drawing.Point(327, 246);
+            this.tbSaveName.Name = "tbSaveName";
+            this.tbSaveName.Size = new System.Drawing.Size(155, 20);
+            this.tbSaveName.TabIndex = 44;
+            // 
+            // tbSavePath
+            // 
+            this.tbSavePath.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbSavePath.Location = new System.Drawing.Point(14, 220);
+            this.tbSavePath.Name = "tbSavePath";
+            this.tbSavePath.Size = new System.Drawing.Size(470, 20);
+            this.tbSavePath.TabIndex = 43;
+            // 
+            // tbSelectExel
+            // 
+            this.tbSelectExel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbSelectExel.Location = new System.Drawing.Point(81, 70);
+            this.tbSelectExel.Name = "tbSelectExel";
+            this.tbSelectExel.Size = new System.Drawing.Size(273, 20);
+            this.tbSelectExel.TabIndex = 42;
+            // 
+            // tbSelectXml
+            // 
+            this.tbSelectXml.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbSelectXml.Location = new System.Drawing.Point(81, 43);
+            this.tbSelectXml.Name = "tbSelectXml";
+            this.tbSelectXml.Size = new System.Drawing.Size(273, 20);
+            this.tbSelectXml.TabIndex = 41;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(14, 51);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 82;
+            this.label6.Text = "Xml folder";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(14, 75);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(49, 13);
+            this.label9.TabIndex = 83;
+            this.label9.Text = "Excel file";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(605, 321);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.tbSelectExel_1);
+            this.Controls.Add(this.tbSelectExel_0);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbBool);
+            this.Controls.Add(this.tbCopyFrom);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnSavePath);
+            this.Controls.Add(this.btnSelectExel);
+            this.Controls.Add(this.btnSelectXml);
+            this.Controls.Add(this.tbSaveName);
+            this.Controls.Add(this.tbSavePath);
+            this.Controls.Add(this.tbSelectExel);
+            this.Controls.Add(this.tbSelectXml);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox tbSelectExel_1;
+        private System.Windows.Forms.TextBox tbSelectExel_0;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbBool;
+        private System.Windows.Forms.TextBox tbCopyFrom;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnSavePath;
+        private System.Windows.Forms.Button btnSelectExel;
+        private System.Windows.Forms.Button btnSelectXml;
+        private System.Windows.Forms.TextBox tbSaveName;
+        private System.Windows.Forms.TextBox tbSavePath;
+        private System.Windows.Forms.TextBox tbSelectExel;
+        private System.Windows.Forms.TextBox tbSelectXml;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Xml_To_Excel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            btnSelectXml.Click += btnSelectXml_Click;
+            btnSelectExel.Click += btnSelectExel_Click;
+            btnSavePath.Click += btnSavePath_Click;
+            btnSave.Click += btnSave_Click;
+        }
+        #region IMainForm
+        public string SelectExelPath
+        {
+            get { return tbSelectXml.Text; }
+        }
+        public string SelectXmlFolderPath
+        {
+            get { return tbSelectExel.Text; }
+        }
+        public string SelectSaveFolderPath
+        {
+            get { return tbSavePath.Text; }
+        }
+        public string SaveName
+        {
+            get { return tbSaveName.Text; }
+        }
+
+        public event EventHandler SelectExel;
+        public event EventHandler SelectXml;
+        public event EventHandler SavePath;
+        public event EventHandler Save;
+        #endregion
+
+        #region Select exel
+        private void btnSelectXml_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                tbSelectXml.Text = dlg.SelectedPath;
+
+                SelectXml?.Invoke(this, EventArgs.Empty);
+            }
+        }
+        private void btnSavePath_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                tbSavePath.Text = dlg.SelectedPath;
+
+                SavePath?.Invoke(this, EventArgs.Empty);
+            }
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            Save?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void btnSelectExel_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "exel|*.xml;*.xlsm|Все файлы|*.*";
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                tbSelectExel.Text = dlg.FileName;
+
+                SelectExel?.Invoke(this, EventArgs.Empty);
+            }
+        }
+        #endregion
+    }
+}
+
