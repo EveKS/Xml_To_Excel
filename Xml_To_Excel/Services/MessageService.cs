9aaf170e4c67a6e36ffeac540934a763485d302d
EveKS <vikkes@yandex.ru>
2016-10-20 13:13:20 +0700

    дабавил MessageService с интерфейсом

---
+++ Xml_To_Excel/Services/MessageService.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Forms;
+
+namespace Xml_To_Excel.Services
+{
+    public interface IMessageService
+    {
+        void ShowError(string error);
+        void ShowExclamation(string exlamation);
+        void ShowMessage(string message);
+    }
+
+    public class MessageService : IMessageService
+    {
+        public void ShowMessage(string message)
+        {
+            MessageBox.Show(message, "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        public void ShowExclamation(string exlamation)
+        {
+            MessageBox.Show(exlamation, "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        public void ShowError(string error)
+        {
+            MessageBox.Show(error, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
